e55666b4dd016efcb1fa882c2b848e68ef48d8fc
s.muthukumar <s.muthukumar@siemens.com>
2021-09-19 12:02:26 +0530

    init

---
+++ src/main/java/com/muthu/spring/batch/DBWriter.java
@@ -0,0 +1,21 @@
+package com.muthu.spring.batch;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DBWriter implements ItemWriter<User> {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void write(List<? extends User> users) throws Exception {
+        System.out.println("Data Saved for Users: " + users);
+
+        userRepository.saveAll(users);
+    }
+}
